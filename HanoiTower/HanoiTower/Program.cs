6aa251bb4dc194ba973345de4c8bd50d2b2bcf49
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2023-11-28 15:58:16 +0100

    variables

--- HanoiTower/HanoiTower/Program.cs
+++ HanoiTower/HanoiTower/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Variablen
+            int numberOfDisks = 3;
+            int numberOfRods = 3;
+            
+
+            List<int[]> disks = new()
+            {
+                new[] {1,1},
+                new[] {1,2},
+                new[] {1,3}
+            };
+
+            List<int[]> rods = new()
+            {
+                new[]{1,0},
+                new[] {2,0},
+                new[] {3,0 }
+            };
+            
+            int[] defaultRod = rods[0];
+            Console.WriteLine(defaultRod[0]); // 1
+
+            bool isGameContinue = true;
+
+
+            //Loops
+            while (isGameContinue)
+            {
+                while (isGameContinue)
+                {
+                    MoveTo();
+                }
+
+
+                HaveYouSucceeded();
+            }
+
+            static void HaveYouSucceeded()
+            {
+                //view
+            }
+
+            static void MoveTo()
+            {
+                //model function
+            }
+
+
+
         }
     }
 }
